19d39820ff8fd65dbf855428f37ec5962ae2b5d0
aa_na <aa_na@LAPTOP-V7KTPFVG>
2019-01-25 16:28:33 -0500

    edfrt

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
@@ -20,7 +20,7 @@ public class StudentArray
         studentList[0] = student1;
         studentList[1] = new StudentDemo("2", "Michael Scott");
         studentList[2] = new StudentDemo("3", "Pam Beasley");
-        studentList[3] = new StudentDemo("4", "Dwight Schrute");
+        studentList[3] = new StudentDemo("4", "abc Schrute");
 
         
         for(int i=0; i<studentList.length; i++)
